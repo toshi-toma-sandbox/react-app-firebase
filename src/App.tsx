6b4a6cd3cacdad1bcb68e593c74d890581d8d361
toshi-toma <toshitoma1217@gmail.com>
2020-09-18 00:05:03 +0900

    firebase init and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is React App with Firebase(toshi-toma).
         </a>
       </header>
     </div>
